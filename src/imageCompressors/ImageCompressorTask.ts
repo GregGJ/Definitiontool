63fdca91b2ac777566723e402391259783a94331
Greg <653623647@qq.com>
2021-06-29 17:42:21 +0800

    效率优化，起飞

--- src/imageCompressors/ImageCompressorTask.ts
+++ src/imageCompressors/ImageCompressorTask.ts
@@ -6,40 +6,41 @@ import * as child_process from "child_process";
 import { CMDData } from "../CMDData";
 import { DrongoEvent } from "../drongo/events/DrongoEvent";
 import { Task } from "../drongo/task/Task";
-const images =require("../node_modules/images/index");
+const images = require("../node_modules/images/index");
 
 
-export class ImageCompressorTask extends Task{
+export class ImageCompressorTask extends Task {
 
-    private __workIndex: number = 0;
     private __workCount: number = 10;
 
-    constructor(){
+    private __workList:Array<{ file: string, md5: string, quality: string }>=[];
+
+    constructor() {
         super();
     }
 
-    start(data?:any):void{
+    start(data?: any): void {
         this.tryNexts();
     }
 
     private tryNexts(): void {
-        if (this.imageList && this.imageList.length != 0) {
-            let file: { file: string, md5: string, quality: string };
-            let extname: string;
-            while (this.__workIndex < this.__workCount&&this.imageList.length) {
-                file = this.imageList.shift()!;
-                this.__workIndex++;
-                extname = path.extname(file.file);
-                extname.toLocaleLowerCase();
-                if (extname == ".png") {
-                    this.__pngCompress(file);
-                } else {
-                    this.__jpgCompress(file);
-                }
-                console.log("图片压缩中："+this.imageList?.length);
-            }
-        } else {
+        if(this.__workList.length==0&&this.imageList.length==0){
             this.dispatchEvent(DrongoEvent.COMPLETE);
+            return;
+        }
+        let file: { file: string, md5: string, quality: string };
+        let extname: string;
+        while (this.__workList.length < this.__workCount && this.imageList.length) {
+            file = this.imageList.shift()!;
+            //添加到工作列表
+            this.__workList.push(file);
+            extname = path.extname(file.file);
+            extname.toLocaleLowerCase();
+            if (extname == ".png") {
+                this.__pngCompress(file);
+            } else {
+                this.__jpgCompress(file);
+            }
         }
     }
 
@@ -55,12 +56,13 @@ export class ImageCompressorTask extends Task{
         cmd += " --speed " + speed;
         cmd += " --force";
         cmd += " --ext -m.png";
+        cmd += " --verbose";
         cmd += " " + input;
 
-        var childProcess:child_process.ChildProcess=child_process.exec(cmd, (err, data) => {
+        var childProcess: child_process.ChildProcess = child_process.execFile(cmd, (err) => {
             if (err) {
                 if (err.message.indexOf("Not a PNG file") >= 0) {
-                    console.warn("Not a PNG file "+file.file);
+                    console.warn("Not a PNG file " + file.file);
                 } else {
                     this.mkDirbyFile(output);
                     fs.copyFileSync(input, output);
@@ -71,13 +73,16 @@ export class ImageCompressorTask extends Task{
                 fs.renameSync(this.input + "/" + m_fileName, output);
                 this.fileConfigs!.set(file.file, file);
             }
-            this.__workIndex--;
+            let index:number=this.__workList.indexOf(file);
+            this.__workList.splice(index,1);
+            
+            console.log("图片压缩完成：" + (this.__workList.length+this.imageList.length) + " " + file.file);
             childProcess.kill();
             this.tryNexts();
         });
     }
 
-    private __jpgCompress(file:  { file: string, md5: string, quality: string }): void {
+    private __jpgCompress(file: { file: string, md5: string, quality: string }): void {
         let output: string = this.output + "/" + file.file;
         this.mkDirbyFile(output);
         let quality: number;
@@ -89,7 +94,11 @@ export class ImageCompressorTask extends Task{
         }
         images(this.input + "/" + file.file).save(output, { quality: quality });
         this.fileConfigs!.set(file.file, file);
-        this.__workIndex--;
+
+        let index:number=this.__workList.indexOf(file);
+        this.__workList.splice(index,1);
+
+        console.log("图片压缩完成：" + (this.__workList.length+this.imageList.length) + " " + file.file);
         this.tryNexts();
     }
 
@@ -105,23 +114,23 @@ export class ImageCompressorTask extends Task{
         fs.mkdirSync(file, { recursive: true });
     }
 
-    private get fileConfigs():Map<string,{ file: string, md5: string, quality: string }>{
+    private get fileConfigs(): Map<string, { file: string, md5: string, quality: string }> {
         return CMDData.data.fileConfigs;
     }
 
-    private get pngquantExe():string{
+    private get pngquantExe(): string {
         return CMDData.data.pngquantExe;
     }
 
-    private get input():string{
+    private get input(): string {
         return CMDData.data.input;
     }
 
-    private get output():string{
+    private get output(): string {
         return CMDData.data.output;
     }
 
-    private get imageList(): Array<{file:string,md5:string,quality:string}>{
+    private get imageList(): Array<{ file: string, md5: string, quality: string }> {
         return CMDData.data.imageList;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { Pipeline } from "./Pipeline";
 
 CMDData.data={};
 CMDData.data.input="D:/erciyuan/clientCS/GameWord/assets";
-CMDData.data.output="D:/erciyuan/clientCS/LowDefinition/assets";
+CMDData.data.output="D:/erciyuan/clientCS/GameWord/definitions/LowDefinition";
 CMDData.data.configPath="D:/erciyuan/clientCS/GameWord/definitions/definitionConfig.json";
 CMDData.data.pngquantExe=path.parse(__dirname).dir+"/tools/pngquant.exe"
 
@@ -17,11 +17,17 @@ class Main{
     constructor(){
         this.pipeline=new Pipeline();
         this.pipeline.addTask(new ImageCompressorPipeline());
+        this.pipeline.addEvent(DrongoEvent.ERROR,this,this.pipelineError);
         this.pipeline.addEvent(DrongoEvent.COMPLETE,this,this.pipelineComplete);
         this.pipeline.start();
     }
     
-    private pipelineComplete():void{
+    private pipelineError(type: string, target?: any, ...arg: any[]):void{
+        console.log(arg);
+        process.exit();
+    }
+    
+    private pipelineComplete(type: string, target?: any, ...arg: any[]):void{
         // console.log(CMDData.data);
         process.exit();
     }
