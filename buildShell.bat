a2ba7a530088f56851d5700d28b00741e11e5dbb
Greg <653623647@qq.com>
2021-06-30 16:39:24 +0800

    脚本更新

--- buildShell.bat
+++ buildShell.bat
@@ -3,4 +3,4 @@ cd /d %~dp0
 set input=%1
 ::工作空间工程地址
 set projectPath=%2
-npm install&&npm run build&&node ./dist/index.js --input %input% --projectPath %projectPath%
+npm install&&npm run build&&node ./dist/index.js -i %input% -p %projectPath%
